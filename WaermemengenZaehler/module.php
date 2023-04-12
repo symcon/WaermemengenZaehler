15bae4792cbaef8bba35c5e623229a15cf7ce2f2
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2023-04-12 12:54:04 +0200

    Add functions to the module

--- WaermemengenZaehler/module.php
+++ WaermemengenZaehler/module.php
@@ -1,23 +1,98 @@
 <?php
 
 declare(strict_types=1);
-	class WaermemengenZaehler extends IPSModule
-	{
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-		}
+    class WaermemengenZaehler extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
+            $this->RegisterPropertyInteger('TemperatureVariableInputID', 1);
+            $this->RegisterPropertyInteger('TemperatureVariableOutputID', 1);
+            $this->RegisterPropertyInteger('WaterMeterID', 1);
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
-	}
+            $this->RegisterTimer('UpdatePower', 15 * 60 * 1000, 'WMZ_UpdatePower($_IPS[\'TARGET\']); WMZ_UpdateEnergy($_IPS[\'TARGET\']);');
+
+            $this->RegisterVariableFloat('Power', $this->Translate('Power'), '~Watt.3680', 0);
+            $this->RegisterVariableFloat('Energy', $this->Translate('Energy'), '~Electricity', 1);
+        }
+
+        public function Destroy()
+        {
+            //Never delete this line!
+            parent::Destroy();
+        }
+
+        public function ApplyChanges()
+        {
+            //Never delete this line!
+            parent::ApplyChanges();
+
+            //Validate the Variables
+            $temperatureIn = $this->ReadPropertyInteger('TemperatureVariableInputID');
+            $temperatureOut = $this->ReadPropertyInteger('TemperatureVariableOutputID');
+            $waterMeter = $this->ReadPropertyInteger('WaterMeterID');
+
+            if (!@IPS_VariableExists($temperatureIn)
+                || !@IPS_VariableExists($temperatureOut)
+                || !@IPS_VariableExists($waterMeter)
+            ) {
+                $this->SetStatus(201); //Some Variables are not exist
+                return;
+            }
+
+            $temperatureInProfile = $this->GetProfile($temperatureIn);
+            $temperatureOutProfile = $this->GetProfile($temperatureOut);
+            $waterMeterProfile = $this->GetProfile($waterMeter);
+
+            if (IPS_GetVariableProfile($temperatureInProfile)['Prefix'] == '°C' || IPS_GetVariableProfile($temperatureOutProfile)['Prefix'] == '°C') {
+                $this->SetStatus(202); //False profile on one of the temperature variables
+                return;
+            }
+            if (IPS_GetVariableProfile($waterMeterProfile)['Prefix'] == 'm³' || IPS_GetVariableProfile($waterMeterProfile)['Prefix'] == 'Liter') {
+                $this->SetStatus(203); //False profile on the water meter variable
+                return;
+            }
+            $this->SetStatus(102);
+
+            //First calculation of the Power and the Energy
+            $this->Update();
+            $this->SetTimerInterval('UpdateWaterMeter', 15 * 60 * 1000);
+        }
+
+        public function UpdatePower()
+        {
+            $temperatureIn = $this->GetValue($this->ReadPropertyInteger('TemperatureVariableInputID'));
+            $temperatureOut = $this->GetValue($this->ReadPropertyInteger('TemperatureVariableOutputID'));
+            $waterMeter = $this->GetValue($this->ReadPropertyInteger('WaterMeterID'));
+
+            //P = (m * c * Delta T) / t
+            //c = 4181 Joule/(Kg*K) <- Specific heat capacity of water
+
+            $power = ($waterMeter * 4181 * ($temperatureIn - $temperatureOut)) / 15 * 60;
+
+            $this->SetValue('Power', $power);
+        }
+
+        public function UpdateEnergy()
+        {
+            $power = $this->GetValue('Power');
+            $temperature = $this->GetValue($this->ReadPropertyInteger('TemperatureVariableInputID')) - $this->GetValue($this->ReadPropertyInteger('TemperatureVariableOutputID'));
+
+            //E = (P * Delta T ) / 3600 * 1000
+            $energy = ($power * $temperature) / 3600 * 1000;
+
+            $this->SetValue('Energy', $energy);
+        }
+
+        private function GetProfile(int $variableID)
+        {
+            $variable = IPS_GetVariable($variableID);
+            if ($variable['VariableCustomProfile'] != '') {
+                return $variable['VariableCustomProfile'];
+            } else {
+                return $variable['VariableProfile'];
+            }
+        }
+    }
